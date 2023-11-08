b4aa6513c62fbea1cafd31663be073724ae58c03
Adan <>
2023-11-08 14:33:01 -0300

    validaciones + readme con guia de usuario y ejemplos

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # MetodoNewton_Python
 Codigo en Python para Matematicas IV del Methodo de Newton
+## Guia de Usuario 
+Guía de Usuario: Aplicación del Método de Newton
+1.	Inicio de la Aplicación:
+   - Asegúrate de que tienes instalado Python y el módulo SymPy en tu entorno de desarrollo.
+	Se descarga de este enlace: https://www.python.org/downloads/
+	Se instala SymPy con este comando: pip install sympy
+   - Ejecuta el programa y se mostrará un mensaje de bienvenida.
+	El comando para ejecutar es: python app.py 
+2. Ingresar la Función:
+   - Debes ingresar una función matemática en la variable `x`, como una expresión algebraica.
+   - Ejemplo válido: `x**2 - 4`.
+   - Ejemplo no válido (error de sintaxis): `2x + 3`.
+   - Ejemplo no válido (función desconocida): `sin(x) / log(x)`.
+3. Ingresar el Valor Inicial (x0):
+   - Ingresa un número real como valor inicial (x0) para comenzar el cálculo.
+   - Ejemplo válido (entero): `5`.
+   - Ejemplo no válido (no numérico): `abc`.
+   - Ejemplo no válido (fuera de rango): `-3` con un rango mínimo permitido de `0`.
+4. Ingresar la Tolerancia Deseada:
+   - Ingresa un número real positivo como la tolerancia deseada.
+   - Ejemplo válido (decimal): `0.001`.
+   - Ejemplo no válido (no numérico): `xyz`.
+   - Ejemplo no válido (fuera de rango): `-0.001` con un rango mínimo permitido de `0`.
+5. Ingresar el Número Máximo de Iteraciones:
+   - Ingresa un número entero positivo como el número máximo de iteraciones permitidas.
+   - Ejemplo válido (entero): `10`.
+   - Ejemplo no válido (no numérico): `abc`.
+   - Ejemplo no válido (fuera de rango): `-5` con un rango mínimo permitido de `1`.
+6. Resultados:
+   - El programa calculará la solución utilizando el Método de Newton.
+   - Mostrará información sobre cada iteración y el resultado final si se encuentra una solución.
+   - Si el método no converge, mostrará un mensaje indicando que no se encontró una solución.
+Esta guía te ayudará a utilizar la aplicación de manera efectiva, validando las entradas y obteniendo resultados precisos utilizando el Método de Newton para encontrar soluciones a ecuaciones no lineales.
+
+## Ejemplos para ingresar
+Ejemplos para ingresar en la aplicación de consola
+Ejemplos completos para ingresar en la aplicación, incluyendo la función, el valor inicial, la tolerancia y el número máximo de iteraciones:
+1. Funciones Lineales:
+   - Función lineal simple: `2*x - 4`.
+   - Valor inicial (x0): `1.5`.
+   - Tolerancia: `0.0001`.
+   - Número máximo de iteraciones: `10`.
+2. Funciones Cuadráticas:
+   - Ecuación cuadrática simple: `x**2 - 9`.
+   - Valor inicial (x0): `3.0`.
+   - Tolerancia: `0.001`.
+   - Número máximo de iteraciones: `15`.
+3. Funciones Trigonométricas:
+   - Función trigonométrica simple: `sin(x) - 0.5`.
+   - Valor inicial (x0): `1.0`.
+   - Tolerancia: `0.0001`.
+   - Número máximo de iteraciones: `10`.
+4. Funciones Exponenciales:
+   - Ecuación exponencial simple: `2*exp(x) - 5`.
+   - Valor inicial (x0): `1.0`.
+   - Tolerancia: `0.001`.
+   - Número máximo de iteraciones: `12`.
+5. Funciones Trascendentales:
+   - Ecuación trascendental simple: `log(x) - 1`.
+   - Valor inicial (x0): `2.0`.
+   - Tolerancia: `0.0001`.
+   - Número máximo de iteraciones: `10`.
+ 
+6. Ecuaciones que no convergen:
+   - Función sin raíces reales: `x**2 + 1`.
+   - Valor inicial (x0): `0.5`.
+   - Tolerancia: `0.0001`.
+   - Número máximo de iteraciones: `15`.
+
+   - Función con raíz múltiple: `(x - 2)**3`.
+   - Valor inicial (x0): `2.0`.
+   - Tolerancia: `0.001`.
+   - Número máximo de iteraciones: `5`.
+
+
--- app.py
+++ app.py
@@ -1,38 +1,63 @@
 import sympy as sp
 
-def validate_input(func):
+def is_valid_function(func):
     try:
-        sp.sympify(func)  # Intenta analizar la función
-    except (sp.SympifyError, sp.DerivativeError) as e:
-        print(f'Error al procesar la función: {e}')
-        return None
-    return func
+        x = sp.symbols('x')
+        f = sp.sympify(func)
+        df = sp.diff(f, x)
+        return True
+    except (sp.SympifyError, sp.SympifyError):
+        return False
+
+def validate_function_input():
+    while True:
+        func = input('Ingrese la función (por ejemplo, x**2 - 4): ')
+        if is_valid_function(func):
+            return func
+        else:
+            print('Error: Función no válida. Por favor, ingrese una función matemática válida.')
+
+def validate_numeric_input(prompt, data_type, min_value=None, max_value=None):
+    while True:
+        try:
+            value = data_type(input(prompt))
+            if (min_value is not None and value < min_value) or (max_value is not None and value > max_value):
+                raise ValueError
+            return value
+        except ValueError:
+            print('Error: Ingrese un valor numérico válido dentro del rango permitido.')
 
 def newton_method(func, x0, tolerance, max_iterations):
-    x = sp.symbols('x')
-    f = sp.sympify(func)
-    df = sp.diff(f, x)
-
     for i in range(max_iterations):
-        f_value = f.subs(x, x0).evalf()
-        df_value = df.subs(x, x0).evalf()
-
-        print(f'\nIteración {i}:')
-        print('==================')
-        print(f'x{i} = {x0}')
-        print(f'f(x{i}) = {f_value}')
-        print(f'f\'(x{i}) = {df_value}')
+        x = sp.symbols('x')
+        try:
+            f = sp.sympify(func)
+            f_value = f.subs(x, x0).evalf()
+        except sp.SympifyError:
+            print('Error: La función no se pudo procesar correctamente.')
+            return None
 
         if abs(f_value) < tolerance:
             print(f'Solución encontrada en la iteración {i}: x = {x0}')
             return x0
 
+        df = sp.diff(f, x)
+        try:
+            df_value = df.subs(x, x0).evalf()
+        except sp.SympifyError:
+            print('Error: La derivada no se pudo calcular correctamente.')
+            return None
+
         if df_value == 0:
-            print('La derivada se hizo cero. No se puede continuar.')
+            print('Error: La derivada se hizo cero. No se puede continuar.')
             return None
 
         x1 = x0 - f_value / df_value
-
+        print(f'\nIteración {i}:')
+        print('==================')
+        print(f'x{i} = {x0}')
+        print(f'f(x{i}) = {f_value}')
+        print(f'f\'(x{i}) = {df_value}')
         print(f'x{i+1} = {x1}')
         print(f'Error absoluto: {abs(x1 - x0)}')
 
@@ -46,17 +71,15 @@ def newton_method(func, x0, tolerance, max_iterations):
     return None
 
 def main():
-    func = input('Ingrese la función (por ejemplo, x**2 - 4): ')
-    x0 = float(input('Ingrese el valor inicial x0: '))
-    tolerance = float(input('Ingrese la tolerancia deseada: '))
-    max_iterations = int(input('Ingrese el número máximo de iteraciones: '))
+    func = validate_function_input()
+    x0 = validate_numeric_input('Ingrese el valor inicial x0: ', float)
+    tolerance = validate_numeric_input('Ingrese la tolerancia deseada: ', float, min_value=0.0)
+    max_iterations = validate_numeric_input('Ingrese el número máximo de iteraciones: ', int, min_value=1)
 
-    validated_func = validate_input(func)
-    if validated_func is not None:
-        result = newton_method(validated_func, x0, tolerance, max_iterations)
+    result = newton_method(func, x0, tolerance, max_iterations)
 
-        if result is not None:
-            print(f'La solución aproximada es x = {result}')
+    if result is not None:
+        print(f'La solución aproximada es x = {result}')
 
 if __name__ == '__main__':
     main()
