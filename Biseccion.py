8fe9e119e2550ee0fada988773da57bed2e436de
Adan <>
2023-11-25 21:31:06 -0300

    cambio de nombre de c por x

--- Biseccion.py
+++ Biseccion.py
@@ -4,23 +4,23 @@ def bisection_method(func, a, b, tol, max_iter):
     x = sp.symbols('x')
     f = sp.lambdify(x, func)
 
-    print("\nIteración\t   a\t\t   b\t\t   c\t\t  f(c)\t\t  b - a")
+    print("\nIteración\t   a\t\t   b\t\t   x\t\t  f(x)\t\t  b - a")
     print("="*80)
 
     iter_count = 0
 
     while iter_count < max_iter:
-        c = (a + b) / 2
-        fc = f(c)
+        x_val = (a + b) / 2
+        fx = f(x_val)
 
-        print(f"{iter_count}\t\t {a:.6f}\t {b:.6f}\t {c:.6f}\t {fc:.6f}\t {b - a:.6f}")
+        print(f"{iter_count}\t\t {a:.6f}\t {b:.6f}\t {x_val:.6f}\t {fx:.6f}\t {b - a:.6f}")
 
-        if fc == 0 or abs(b - a) < tol:
+        if fx == 0 or abs(b - a) < tol:
             break
-        elif f(a) * fc < 0:
-            b = c
+        elif f(a) * fx < 0:
+            b = x_val
         else:
-            a = c
+            a = x_val
         iter_count += 1
 
     root_approximation = (a + b) / 2
@@ -46,7 +46,7 @@ def main():
         func, a, b, tolerance, max_iterations = user_input
         print("\nResolviendo usando el método de la bisección:")
         print("=====================================================")
-        print("Iteración\t   a\t\t   b\t\t   c\t\t  f(c)\t\t  b - a")
+        print("Iteración\t   a\t\t   b\t\t   x\t\t  f(x)\t\t  b - a")
         print("="*80)
         root, iterations = bisection_method(func, a, b, tolerance, max_iterations)
         print("="*80)
